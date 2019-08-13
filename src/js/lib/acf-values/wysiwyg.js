962014d6b836f63c9602a288cca94b50ac3c3e27
mcguffin <joern@flyingletters.com>
2019-08-13 13:03:04 +0200

    fix wysiwyg not duplicating <br>

--- src/js/lib/acf-values/wysiwyg.js
+++ src/js/lib/acf-values/wysiwyg.js
@@ -3,17 +3,21 @@ import $ from 'jquery';
 module.exports = ( $src, $dest ) => { // ...
 	const srcField = acf.getField($src),
 		destField = acf.getField($dest),
-		dest_id = destField.$input().attr('id'),
-		val = srcField.getValue();
+		src_id = srcField.$input().attr('id'),
+		dest_id = destField.$input().attr('id');
+	let val;
+	let srcEditor = tinymce.get( src_id );
+	let destEditor = tinymce.get( dest_id );
 
-	let editor = tinymce.get( dest_id );
-
-	if ( !! editor ) {
+	if ( !! destEditor && !! srcEditor ) {
+		val = srcEditor.getContent()
 		// editor already inited
-		editor.setContent( val );
+		destEditor.setContent( val );
 		$dest.find('textarea').val( $src.find('textarea').val() )
-		editor.save();
+		destEditor.save();
+		console.log(val);
 	} else {
+		val = srcField.getValue();
 		// delayed init
 		destField.$input().html( val );
 	}
